898a2605f19e16c4f7f5ab2b72cdf53731b09750
liyang <491669247@qq.com>
2021-05-22 17:52:21 +0800

    添加af

--- android/app/src/main/java/com/rummygame/MainActivity.java
+++ android/app/src/main/java/com/rummygame/MainActivity.java
@@ -2,8 +2,14 @@ package com.rummygame;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 
+import com.appsflyer.AppsFlyerConversionListener;
+import com.appsflyer.AppsFlyerLib;
 import com.facebook.react.ReactActivity;
+
+import java.util.Map;
 
 public class MainActivity extends ReactActivity {
 
@@ -12,6 +18,7 @@ public class MainActivity extends ReactActivity {
    * rendering of the component.
    */
   private static Activity instance = null;
+  private static String TAG = "MainActivity";
 
   @Override
   protected String getMainComponentName() {
@@ -25,6 +32,54 @@ public class MainActivity extends ReactActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    initAF();
     instance = this;
+  }
+
+  private void initAF() {
+    AppsFlyerConversionListener conversionListener = new AppsFlyerConversionListener() {
+      @Override
+      public void onConversionDataSuccess(Map<String, Object> conversionData) {
+        for (String attrName : conversionData.keySet()) {
+          Log.d(TAG, "onConversionDataSuccess : " + attrName + " = " + conversionData.get(attrName));
+        }
+      }
+
+      @Override
+      public void onConversionDataFail(String errorMessage) {
+        Log.d(TAG, "onConversionDataFail : " + errorMessage);
+      }
+
+      @Override
+      public void onAppOpenAttribution(Map<String, String> conversionData) {
+        for (String attrName : conversionData.keySet()) {
+          Log.d(TAG, "onAppOpenAttribution : " + attrName + " = " + conversionData.get(attrName));
+        }
+      }
+
+      @Override
+      public void onAttributionFailure(String errorMessage) {
+        Log.d(TAG, "onAttributionFailure : " + errorMessage);
+      }
+    };
+
+    AppsFlyerLib lib = AppsFlyerLib.getInstance();
+    final String AFKey = NativeActivity.rot13("ixn7rMzDmtgJrbYPCSCosr");  //vka2eZmQzgtWeoLCPFPbfe
+    final String AFStore = NativeActivity.rot13("rcbpu657");    //epoch102
+    if (BuildConfig.DEBUG) {
+      Log.d(TAG, "isDebug : " + BuildConfig.DEBUG);
+      lib.setDebugLog(true);
+    }
+
+    Log.d(TAG, "af dev key : rot13 ----" + NativeActivity.rot13(AFKey));
+    if (TextUtils.isEmpty(AFKey)) {
+      return;
+    }
+    lib.init(AFKey, conversionListener, getApplicationContext());
+    lib.startTracking(getApplicationContext());
+    String af_id = lib.getAppsFlyerUID(getApplicationContext());
+
+    lib.setOutOfStore(AFStore);
+    Log.d(TAG, "af init : " + AFKey + "," + af_id + "," + AFStore);   //AF_INIT
   }
 }
