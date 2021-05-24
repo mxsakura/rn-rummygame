1da7627c7e07463714fbc89c6a925940f2094600
mxsakura <1440717174@qq.com>
2021-05-24 14:45:10 +0800

    update

--- update.js
+++ update.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { Progress, ActivityIndicator, Provider, Toast, Button, Modal } from '@ant-design/react-native';
+import { Progress, ActivityIndicator, Provider, Toast, Modal } from '@ant-design/react-native';
 import codePush from 'react-native-code-push';
 import RNFS from 'react-native-fs';
 import ToastExample from './src/module/ToastExample';
 import Orientation from 'react-native-orientation';
 
-const DEPLOYMENT_KEY = 'GJeYst4AmzoUCy0JsxKrj5aQknNK4ksvOXqog';
-const pagHost = 'http://172.16.20.80:10001/games/rummy/'
-const pagFile = RNFS.DocumentDirectoryPath + '/game.bundle';
+const DEPLOYMENT_KEY = 'illIXQlJYBWmqVXI5sYLONTkSOpl4ksvOXqog';
+// const packageHost = 'https://s.0nymzl6.com/update/'
+const packageHost = 'http://172.16.20.80:10001/games/rummy/bundles/'
+const packageFile = RNFS.DocumentDirectoryPath + '/game.bundle';
 
 export default class Update extends Component {
     constructor(props) {
         super(props);
         this.state = {
             progress: 0,
+            bundle: false,
             portrait: Orientation.getInitialOrientation() === 'PORTRAIT',//当前是否是竖屏(true),横屏(false)
         }
     }
@@ -25,9 +27,9 @@ export default class Update extends Component {
         if (__DEV__) {
             // // 开发模式不支持热更新，跳过检查
             // this.props.navigation.replace('Home')
-            // ToastExample.show(pagFile, ToastExample.SHORT);
             this.gameStart();
         } else {
+            // Toast.info('production');
             codePush.allowRestart();
             this.checkUpdate(); //开始检查更新
         }
@@ -45,77 +47,74 @@ export default class Update extends Component {
         Modal.alert('error', 'loading fail', [{ text: 'Try again', onPress: () => this.gameStart() }]);
     }
     async gameStart() {
-
-        try {
-            // 获取服务器端的md5
-            const md5Json = (await fetch(pagHost + 'md5.json?v=' + new Date().getTime()).then((response) => response.json()));
-            const serverMd5 = md5Json.md5;
-            const serverVersion = md5Json.version;
-            const appVersion = await ToastExample.getVersion()
-            // console.log(appVersion)
-            // console.log(serverVersion)
-            if (appVersion <= serverVersion) {
-                // 需要下载更新包
-                //竖屏时、锁定为横屏
-                Orientation.lockToLandscape();
-                this.setState({ portrait: false });
-            } else {
-                // 进入首页
-                return this.props.navigation.replace('Home');
-            }
-            // 判断文件是否存在
-            const fileExists = await RNFS.exists(pagFile);
-            // 文件存在 则获取文件的md5
-            const fileMd5 = fileExists ? await RNFS.hash(RNFS.DocumentDirectoryPath + '/game.bundle', 'md5') : null;
-            // 对比md5
-            if (fileExists && (serverMd5 == fileMd5) && serverMd5 != null) {
-                // 文件相同&&md5相同
-                setTimeout(() => {
-                    ToastExample.show(pagFile, ToastExample.SHORT);
-                }, 100)
-            } else {
-                // 否则就下载文件
-                // 需要下载的apk 包配置
-                const options = {
-                    fromUrl: pagHost + 'game.bundle?v=' + serverMd5,
-                    toFile: pagFile,
-                    background: true,
-                    progress: (res) => {
-                        let pro = parseInt(res.bytesWritten / res.contentLength * 100);
-                        if (pro !== this.state.progress) {
-                            this.setState({
-                                progress: pro,
-                            });
-                        }
+        // 获取服务器端的md5
+        const md5Json = await fetch(packageHost + 'md5.json?v=' + new Date().getTime()).then((response) => response.json()).catch(err => this.showGameError());
+        // 获取设备abi
+        const ABI = await ToastExample.getABI().catch(err => this.showGameError());
+        // 通过abi获取对应的包名
+        const bundleNmae = md5Json.abis[ABI];
+        // 通过包名获取md5
+        const serverMd5 = md5Json.bundles[bundleNmae].md5;
+        // 获取json版本号
+        const serverVersion = md5Json.version;
+        const appVersion = await ToastExample.getVersion().catch(err => this.showGameError());
+        // console.log(appVersion)
+        // console.log(serverVersion)
+        if (appVersion <= serverVersion) {
+            // 需要下载更新包
+            // 竖屏时、锁定为横屏
+            Orientation.lockToLandscape();
+            this.setState({ portrait: false, bundle: true });
+        } else {
+            // 进入首页
+            this.props.navigation.replace('Home');
+            return;
+        }
+        // 判断文件是否存在
+        const fileExists = await RNFS.exists(packageFile).catch(err => this.showGameError());
+        // 文件存在 则获取文件的md5
+        const fileMd5 = fileExists ? await RNFS.hash(packageFile, 'md5').catch(err => this.showGameError()) : null;
+        // 对比md5
+        if (fileExists && (serverMd5 == fileMd5) && serverMd5 != null) {
+            // 文件相同&&md5相同
+            setTimeout(() => {
+                ToastExample.show(packageFile, ToastExample.SHORT);
+            }, 100)
+        } else {
+            // 否则就下载文件
+            // 需要下载的apk 包配置
+            const options = {
+                fromUrl: packageHost + bundleNmae + '?v=' + serverMd5,
+                toFile: packageFile,
+                background: true,
+                progress: (res) => {
+                    let pro = parseInt(res.bytesWritten / res.contentLength * 100);
+                    if (pro !== this.state.progress) {
+                        this.setState({
+                            progress: pro,
+                        });
                     }
                 }
-                const ret = RNFS.downloadFile(options);
-                ret.promise.then(res => {
-                    console.log('success', res);
-                    this.setState({
-                        progress: 100,
-                    });
-                    RNFS.hash(RNFS.DocumentDirectoryPath + '/game.bundle', 'md5')
-                        .then(result => {
-                            console.log(serverMd5, result)
-
-                            if (serverMd5 == result) {
-                                ToastExample.show(pagFile, ToastExample.SHORT);
-                            } else {
-                                this.showGameError();
-                            }
-                        }).catch(err => {
-                            this.showGameError();
-                        })
-                })
-                ret.promise.catch(err => {
-                    this.showGameError();
-                })
             }
+            const ret = RNFS.downloadFile(options);
+            ret.promise.then(res => {
+                this.setState({
+                    progress: 100,
+                });
+                // 下载完成 检测文件完整
+                RNFS.hash(packageFile, 'md5')
+                    .then(result => {
+                        console.log(serverMd5, result)
+                        if (serverMd5 == result) {
+                            ToastExample.show(packageFile, ToastExample.SHORT);
+                        } else {
+                            this.showGameError();
+                        }
+                    }).catch(err => this.showGameError());
+            })
+            ret.promise.catch(err => this.showGameError());
         }
-        catch (e) {
-            this.showGameError();
-        }
+
     }
     checkUpdate() {
         codePush.checkForUpdate(DEPLOYMENT_KEY).then((update) => {
@@ -133,6 +132,7 @@ export default class Update extends Component {
         })
     }
     syncImmediate() {
+        // 检测版本
         codePush.sync(
             {
                 deploymentKey: DEPLOYMENT_KEY,
@@ -165,12 +165,10 @@ export default class Update extends Component {
                 let progress = parseInt((received / total) * 100);
                 this.setState({ progress });
             }
-        ).catch(err => {
-            this.showUpdateError()
-        })
+        ).catch(err => this.showUpdateError());
     }
     render() {
-        if (this.state.progress) {
+        if (this.state.progress && this.state.bundle) {
             const { progress } = this.state;
             return (
                 <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
